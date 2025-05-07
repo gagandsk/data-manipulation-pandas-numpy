773cad5a991ad6661c55f0317e9c4d39ae6bfd08
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-05-07 12:10:17 +0200

    filter with 'loc' & 'iloc'

--- pandas/load_files.py
+++ pandas/load_files.py
@@ -2,8 +2,26 @@ import pandas as pd
 
 url = "https://static.platzi.com/media/public/uploads/hpcharactersdataraw_3d934e85-dfa4-42ec-8520-fadfbecae574.json"
 
-df_books = pd.read_csv('bestsellers.csv', sep=',', header=0, names=['Names', 'Authors', 'User Rating', 'Reviews', 'Price', 'Year', 'Genre'])
+df_books = pd.read_csv('bestsellers.csv', sep=',', header=0, names=['Name', 'Author', 'User Rating', 'Review', 'Price', 'Year', 'Genre'])
 print(df_books)
 
 print("---> JSON <--- ")
 print(pd.read_json('HPCharactersData.json', typ='Series'))
+
+print(df_books[0:4])
+print(df_books['Name'])
+print(df_books[['Name', 'Author', 'Year']])
+
+#loc
+print(df_books.loc[:])
+print(df_books.loc[0:4])
+print(df_books.loc[0:2, ['Name', 'Author']])
+print(df_books.loc[:, ['Review']] * -1)
+print(df_books.loc[0:2, ['Author']] == 'JJ Smith')
+
+#iloc
+print('\n ---> ILOC <--- \n')
+print(df_books.iloc[:])
+print(df_books.iloc[:, 0:3])
+print(df_books.iloc[1:3] * -2)
+print(df_books.iloc[:2, 2:])
